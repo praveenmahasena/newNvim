4b804db24790eea27301c93cc1c45d2e0d94b8f7
praveenmahasena <praveenmahasena647@gmail.com>
2024-05-31 12:25:00 -0400

    adding color theme

--- lua/maya/lazy/colors.lua
+++ lua/maya/lazy/colors.lua
@@ -80,5 +80,58 @@ return{
     },
     {
         'maxmx03/fluoromachine.nvim',
-    }
+        config=function ()
+
+        end
+    },
+    {
+        "maxmx03/solarized.nvim",
+        lazy = false,
+        priority = 1000,
+        config=function ()
+            require('solarized').setup({
+                transparent = false, -- enable transparent background
+                palette = 'solarized', -- or selenized
+                styles = {
+                    comments = {},
+                    functions = {},
+                    variables = {},
+                    numbers = {},
+                    constants = {},
+                    parameters = {},
+                    keywords = {},
+                    types = {},
+                },
+                enables = {
+                    bufferline = true,
+                    cmp = true,
+                    diagnostic = true,
+                    dashboard = true,
+                    editor = true,
+                    gitsign = true,
+                    hop = true,
+                    indentblankline = true,
+                    lsp = true,
+                    lspsaga = true,
+                    navic = true,
+                    neogit = true,
+                    neotree = true,
+                    notify = true,
+                    noice = true,
+                    semantic = true,
+                    syntax = true,
+                    telescope = true,
+                    tree = true,
+                    treesitter = true,
+                    todo = true,
+                    whichkey = true,
+                    mini = true,
+                },
+                highlights = {},
+                colors = {},
+                theme = 'default', -- or 'neo'
+                autocmd = true,
+            })
+        end
+    },
 }
